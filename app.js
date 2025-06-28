599f6d3e001b1e6017ffdb04ca98879089b84372
Renz <renzonifico@gmail.com>
2025-06-28 19:48:00 +0800

    Login Page + Server Skeleton

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+const hbs = require("handlebars")
+const path = require('path')
+
+const PORT = process.env.port || 3000
+
+// Templates and Static Files
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'templates'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+    res.render("login")
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
