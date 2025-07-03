68da745a5da59c0284a5bed78e65ea27760b7a9f
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2025-07-03 19:44:09 +0800

    UPDATED: App.js to include routes & mongoDBconnect

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express")
 const app = express()
 const hbs = require("handlebars")
 const path = require('path')
+const authRoutes = require('./routes/authRoutes')
+const connectDB = require('./config/connect')
 
-const PORT = process.env.port || 3000
+
+const PORT = process.env.PORT || 3000
+
+connectDB();
 
 // Templates and Static Files
 app.set('view engine', 'ejs');
@@ -13,6 +18,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", (req, res) => {
     res.render("login")
 })
+
+
+app.use('/auth',authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
