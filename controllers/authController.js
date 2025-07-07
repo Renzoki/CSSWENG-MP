9aa5808a2c98a631e70c92056214e978591c4a9f
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2025-07-07 14:05:21 +0800

    UPDATED: Login logic fixed, fetches data from DB

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,29 @@
+const User = require('../models/user')
+
+
 //TODO: Login logic
-
-exports.login = (req,res) => {
+exports.login = async (req,res) => {
     const { email, password} = req.body;
+    try{
+        const user = await User.findOne({email})
 
-    //Temporary
-    if (email === "admin@gmail.com" && password === "admin") {
-        res.status(200).json({ message: "Login successful!" })
-    }else
-        res.status(401).json({ message: "Invalid credentials." })
+        if(!user){
+            return res.status(401).json({ message: "User not found!"})
+        }
+
+        const isEqual = await user.comparePassword(password)
+        
+        if(!isEqual){
+            return res.status(401).json({ message: "Incorrect password!"})
+        }else{
+            return res.status(200).json({ message: "Login successful!", redirect: "drafts" }) 
+        }
+    
+    }catch (error){
+        console.error("Login error: ",error);
+        return res.status(500).json({ message: "Server Error!"})
+
+    }
 };
 
 exports.forgot_password = (req,res) => {
