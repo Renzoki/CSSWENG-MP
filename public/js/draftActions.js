96fe19f9f071cdb4848c776ae79b485cb8b58fc7
Renz <renzonifico@gmail.com>
2025-07-07 16:39:02 +0800

    New confirm popup before duplicating & deleting articles

--- public/js/draftActions.js
+++ public/js/draftActions.js
@@ -47,10 +47,7 @@ listContainer.addEventListener("click", (e) => {
         if (listContainer.dataset.version === "original") { //copy to copyList while duplicating in original
             copyOfList = Array.from(listContainer.children).map(child => child.cloneNode(true));
         } else if (listContainer.dataset.version === "copy") { //copy to original while duplicating during search
-            console.log(copyOfList)
             duplicateItem(e.target.parentNode.parentNode, "copy")
-            console.log(copyOfList)
-
         }
     }
 
@@ -61,16 +58,44 @@ listContainer.addEventListener("click", (e) => {
     }
 })
 
-// DUPLICATE AND DELETE ITEMS LOGIC 
+// DUPLICATE, DELETE, and CONFIRMATION LOGIC
+const modalContainer = document.querySelector('#modal-overlay');
+const modalBox = document.querySelector("#modal")
+const yes = document.querySelector("#confirmation-yes")
+const no = document.querySelector("#confirmation-no")
+
 listContainer.addEventListener("click", (e) => {
-    if (e.target.className.includes("duplicate-icon")) {
-        duplicateItem(e.target.parentNode.parentNode, "original")
+    if (e.target.className.includes("duplicate-icon")) { 
+        actionController(e, duplicateItem, "duplicate") //confirm and duplicate
     }
 
-    if (e.target.className.includes("delete-icon")) {
-        deleteItem(e.target.parentNode.parentNode, "original")
+    if (e.target.className.includes("delete-icon")) { //confirm and delete
+        actionController(e, deleteItem, "delete")
     }
 })
+
+const actionController = (e, func, action) => {
+    const element = e.target.parentNode.parentNode //get element
+    const articleTitle = element.querySelector(".article-item") //get title 
+    const actionName = document.querySelector("#action-container-span") 
+    const popupArticleName = document.querySelector("#article-title-span")
+
+    actionName.textContent = action
+    popupArticleName.textContent = articleTitle.textContent //assign title to popup
+    modalContainer.classList.remove('hidden'); //show popup
+
+    modalContainer.addEventListener('click', w => {
+        if (w.target === yes) {
+            func(element, "original")
+            modalContainer.classList.add('hidden');
+        } else if (w.target === no) {
+            modalContainer.classList.add('hidden');
+        }
+        else if (w.target === modalContainer) {
+            modalContainer.classList.add('hidden');
+        }
+    }, { once: true })
+}
 
 const duplicateItem = (originalItem, version) => {
     if (version === "original") {
