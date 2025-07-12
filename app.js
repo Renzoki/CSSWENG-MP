ff11165bb9d2e90eb3685fde70d7a70094db4319
Renz <renzonifico@gmail.com>
2025-07-12 19:46:58 +0800

    NEW: Organized Files

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'templates'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-    res.render("login")
+    res.render("create")
 })
 
 app.listen(PORT, () => {
