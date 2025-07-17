5bf55303062d95d6d54d1321587e7251939a516d
QAZYD <simondioresambata@gmail.com>
2025-07-17 13:40:24 +0800

    UPDATE: fix route bug

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router()
 router.get('/',articleController.getAllArticles);
 router.get('/:id',articleController.getArticleById);
 router.post('/', articleController.createArticle);
-router.patch('/:id', articleController.updateArticle);s
+router.patch('/:id', articleController.updateArticle);
 router.delete('/:id', articleController.deleteArticle);
 
 
