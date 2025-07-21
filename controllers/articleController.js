77d66a9d22cb9a825c413e7e23157ce59cdebe02
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2025-07-21 17:21:47 +0800

    FIXED: indentation

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,7 +1,7 @@
-        const Article = require('../models/article')
-        const mongoose = require('mongoose');
+const Article = require('../models/article')
+const mongoose = require('mongoose');
 
-        // creates an article on the database
+// creates an article on the database
 exports.createArticle = async (req, res) => {
   try {
     const { title, author, status, publish_date, blocks } = req.body;
@@ -32,37 +32,37 @@ exports.createArticle = async (req, res) => {
   }
 };
 
-        // update article on the db 
-        exports.updateArticle = async (req,res) =>{
-            try{
-                const { id } = req.params;
-                const updateData = req.body;
+// update article on the db 
+exports.updateArticle = async (req,res) =>{
+    try{
+        const { id } = req.params;
+        const updateData = req.body;
 
-                if(updateData.status){
-                    const validStatuses = ['posted', 'unfinished', 'finished'];
-                    if(!validStatuses.includes(updateData.status)){
-                        return res.status(400).json({message: 'Invalid Status Value.'});
-                    }
-                }
-            
-
-            const updateArticle = await Article.findByIdAndUpdate(
-                id,
-                {$set: updateData},
-                {new: true, runValidators: true}
-            );
-
-            if (!updateArticle){
-                return res.status(404).json({message: 'Article not found.'});
+        if(updateData.status){
+            const validStatuses = ['posted', 'unfinished', 'finished'];
+            if(!validStatuses.includes(updateData.status)){
+                return res.status(400).json({message: 'Invalid Status Value.'});
             }
-
-            return res.status(200).json(updateArticle);
-            } catch(err){
-                console.error('Error updating Articles:', err);
-                return res.status(500).json({message: 'Server error while updating article.'});
-            }
-
         }
+    
+
+    const updateArticle = await Article.findByIdAndUpdate(
+        id,
+        {$set: updateData},
+        {new: true, runValidators: true}
+    );
+
+    if (!updateArticle){
+        return res.status(404).json({message: 'Article not found.'});
+    }
+
+    return res.status(200).json(updateArticle);
+    } catch(err){
+        console.error('Error updating Articles:', err);
+        return res.status(500).json({message: 'Server error while updating article.'});
+    }
+
+}
 
 //deletes article
 exports.deleteArticle = async (req, res) => {
@@ -87,7 +87,7 @@ exports.deleteArticle = async (req, res) => {
 };
 
 
-        //returns all articles
+//returns all articles
 exports.getAllArticles = async (req, res) => {
   try {
     const articles = await Article.find();
@@ -98,7 +98,7 @@ exports.getAllArticles = async (req, res) => {
   }
 };
 
-        //TODO: get the content of a specific article, for uploading/editing
+//get the content of a specific article, for uploading/editing
 
 exports.getArticleById = async (req, res) => {
   try {
