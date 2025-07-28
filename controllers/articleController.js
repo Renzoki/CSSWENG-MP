e66b4d71e913c655fa9091350dbfff6898c06c61
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2025-07-28 11:53:48 +0800

    UPDATED: Changed valid statuses

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -10,7 +10,7 @@ exports.createArticle = async (req, res) => {
       return res.status(400).json({ message: 'Missing required fields' });
     }
 
-    const validStatuses = ['posted', 'unfinished', 'finished','archived'];
+    const validStatuses = ['published', 'unfinished', 'finished','archived'];
     if (!validStatuses.includes(status)) {
       return res.status(400).json({ message: 'Invalid Status Value.' });
     }
